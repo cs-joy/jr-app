ee222bc76e7c60a76920409e2e1c36134df66323
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-03-06 00:18:05 +0600

    first commit

---
+++ studensystemclient/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ButtonAppBar from './components/Appbar';
+import Student from './components/Student';
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonAppBar/>
+      <Student />
+    </div>
+  );
+}
+
+export default App;
